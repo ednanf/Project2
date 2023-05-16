5d37356fb4caf61b6f0fc6ab4c0aa451642f7844
Ednan <52186220+ednanf@users.noreply.github.com>
2023-05-16 09:23:45 -0300

    Part 3 completed.

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -9,15 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+// MARK: - IBOutlets
     @IBOutlet var button1: UIButton!
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
     
+    
+// MARK: - Properties
+    
+    var countries: [String] = []
+    var score = 0
+    
+    
+// MARK: - Setup
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Populate the array with countries -- the names matches the filenames.
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        // Adds a border to the flags so the black/white won't blend into the background.
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        // Changes the border's color
+        // CALayer doesn't understand UIColor because it's on a lower level. However, it understands CGColor (Core Graphics).
+        // UIColor.lightGray sets the color to light gray, and .cgColor converts that value so CAlayer can understand.
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
+        
     }
 
+    
+// MARK: - Functions
 
+    // Show 3 random flags
+    func askQuestion() {
+        // button.setImage assigns a UIImage to the button. The image's name is the position in the array.
+        // for: .normal means the button's state that should be changed is the standard state of the button.
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
+    
+    
 }
 
